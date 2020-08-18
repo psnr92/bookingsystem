877eedfdf8612f6b368d9eb05a7895882624e4c9
saenger <philipp.saenger@gmx.de>
2020-08-18 13:29:02 +0200

    Added allauth and managed booking_list permissions

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -11,6 +11,13 @@ class RoomList(ListView):
 
 class BookingList(ListView):
     model=Booking
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_staff:
+            booking_list = Booking.objects.all()
+            return booking_list
+        else:
+            booking_list = Booking.objects.filter(user=self.request.user)
+            return booking_list
 
 class BookingView(FormView):
     form_class = AvailabilityForm
