941124e7d6312465c619832d35b46f48c264868d
saenger <philipp.saenger@gmx.de>
2020-08-20 11:52:22 +0200

    Custom User Registration Django (AbstractBaseUser and UserCreationForm)

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,13 @@ from personal.views import (
     home_screen_view
 )
 
+from account.views import (
+    registration_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
+    path('register/', registration_view, name="register"),
     #path('accounts/', include('allauth.urls'),)
 ]
